3536439398c28298586a9ed6abc0acda3a213734
AmadeoMartell <medeu207@gmail.com>
2025-05-31 23:40:55 +0500

    Added prometheus connection via Docker

--- src/main/java/com/epam/capstone/api/v1/MetricsController.java
+++ src/main/java/com/epam/capstone/api/v1/MetricsController.java
@@ -2,6 +2,7 @@ package com.epam.capstone.api.v1;
 
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MetricsController {
     private final PrometheusMeterRegistry prometheusRegistry;
+
     @Autowired
     public MetricsController(PrometheusMeterRegistry prometheusRegistry) {
         this.prometheusRegistry = prometheusRegistry;
     }
-    @GetMapping("/metrics")
+
+    @GetMapping(value = "/metrics", produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String scrape() {
         return prometheusRegistry.scrape();
     }
+
 }
 
